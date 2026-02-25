0dfd059d53d61bb8db4d88622cbcf44e73c2ad33
Ettore <ettore.paolo.2008@gmail.com>
2026-02-25 16:25:58 +0100

    Revert "added a comment"
    This reverts commit ad88bfc119d993c5a2d4740786520a4965180dba.

--- ListeExample copia/src/App.java
+++ ListeExample copia/src/App.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        //dichiaro una lista staticamente.
+
         Lista L1 = new Lista();
 
 
